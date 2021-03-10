48e4f5122299c963e0975d242dbf70def1668fcb
havalenciam <80369599+havalenciam@users.noreply.github.com>
2021-03-09 20:34:25 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Mi primer mensaje!</h1>
+<div>
+  Este es mi primer Mensaje
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
